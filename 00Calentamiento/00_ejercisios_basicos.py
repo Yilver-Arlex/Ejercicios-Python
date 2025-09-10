c2cdd9200ec9d0d623ab9019e8fb8578f422f546
Yilver <yilverarlexjovelangarita@gmail.com>
2025-09-10 04:40:10 -0500

    Revición y Prints

---
+++ 00Calentamiento/00_ejercisios_basicos.py
@@ -0,0 +1,5 @@
+################### Ejercicios Basicos para desperta ###########################
+
+print("despierta")
+print("El compu esta un poco lento sierto")
+print("La paciencia es lo que mas cuesta")
